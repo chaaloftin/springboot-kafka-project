d302c351cbad6b451eb8529545abb683ec00ec23
Chaa Loftin <chaamail@yahoo.com>
2023-02-26 12:10:19 -0500

    Created new Kafka Topic to ingest JSON data

--- src/main/java/com/example/springbootkafkatutorial/config/KafkaTopicConfig.java
+++ src/main/java/com/example/springbootkafkatutorial/config/KafkaTopicConfig.java
@@ -9,10 +9,18 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaTopicConfig {
     public static final String TOPIC_NAME = "test-topic";
+    public static final String JSON_TOPIC_NAME = "json-test-topic";
     @Bean
     public NewTopic myNewTopic()  {
         return TopicBuilder
                 .name(TOPIC_NAME)
+                .build();
+    }
+
+    @Bean
+    public NewTopic myNewJSONTopic()  {
+        return TopicBuilder
+                .name(JSON_TOPIC_NAME)
                 .build();
     }
 }
