57d44e21598cd7f04058887c407a2883722b2929
Chaa Loftin <chaamail@yahoo.com>
2023-02-26 11:50:41 -0500

    created KafkaJSONProduer to send JSON as POJO to Kafka Topics

--- src/main/java/com/example/springbootkafkatutorial/kafka/KafkaJSONProducer.java
+++ src/main/java/com/example/springbootkafkatutorial/kafka/KafkaJSONProducer.java
@@ -1,10 +1,30 @@
 package com.example.springbootkafkatutorial.kafka;
 
+import com.example.springbootkafkatutorial.config.KafkaTopicConfig;
+import com.example.springbootkafkatutorial.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class KafkaJSONProducer {
+    private KafkaTemplate<String, User> kafkaTemplate;
 
+    public KafkaJSONProducer(KafkaTemplate<String, User> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(User data) {
+        log.info("Sending Message: " + data.toString());
+
+        Message<User> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, KafkaTopicConfig.TOPIC_NAME)
+                .build();
+        kafkaTemplate.send(message);
+    }
 }
