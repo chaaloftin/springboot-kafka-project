da3674154fbe8ce1df2eae4d2eb56679c9d22c08
Chaa Loftin <chaamail@yahoo.com>
2023-02-26 11:42:54 -0500

    created KafkaJSONProducer and User Data classes

--- src/main/java/com/example/springbootkafkatutorial/kafka/KafkaConsumer.java
+++ src/main/java/com/example/springbootkafkatutorial/kafka/KafkaConsumer.java
@@ -1,16 +1,18 @@
 package com.example.springbootkafkatutorial.kafka;
 
 import com.example.springbootkafkatutorial.config.KafkaTopicConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class KafkaConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
     @KafkaListener(topics = KafkaTopicConfig.TOPIC_NAME, groupId = "myGroup")
     public void consumer(final String message) {
-        LOGGER.info("messaged received: " + message);
+        log.info("messaged received: " + message);
     }
 }
--- src/main/java/com/example/springbootkafkatutorial/kafka/KafkaProducer.java
+++ src/main/java/com/example/springbootkafkatutorial/kafka/KafkaProducer.java
@@ -1,15 +1,13 @@
 package com.example.springbootkafkatutorial.kafka;
 
 import com.example.springbootkafkatutorial.config.KafkaTopicConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class KafkaProducer {
-    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
@@ -17,7 +15,7 @@ public class KafkaProducer {
     }
 
     public void sendMessage(final String message) {
-        LOGGER.info("Sending message : " +  message);
+        log.info("Sending message : " +  message);
         kafkaTemplate.send(KafkaTopicConfig.TOPIC_NAME, message);
     }
 }
